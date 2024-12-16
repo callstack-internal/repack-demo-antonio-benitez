6f829bcfbd3fbef4957aed1ac1dfd3a891d9c8d8
Antonio Benitez <mrsasuu@gmail.com>
2024-12-16 16:39:26 +0100

    Add ScriptManager

--- RNRepack/index.js
+++ RNRepack/index.js
@@ -5,5 +5,15 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import {ScriptManager, Script} from '@callstack/repack/client';
+
+ScriptManager.shared.addResolver(async scriptId => {
+  if (__DEV__) {
+    return {
+      url: Script.getDevServerURL(scriptId),
+      cache: false,
+    };
+  }
+});
 
 AppRegistry.registerComponent(appName, () => App);
